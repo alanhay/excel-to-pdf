6c17df8d7d532934508545c6c382a1123a6475cb
Alan Hay <alanhay99@hotmail.com>
2017-01-10 19:27:40 +0000

    Write out generated HTML.

--- src/main/java/uk/co/certait/test/ExcelToPdfConverter.java
+++ src/main/java/uk/co/certait/test/ExcelToPdfConverter.java
@@ -2,6 +2,12 @@ package uk.co.certait.test;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
+
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
 import org.apache.poi.hssf.converter.ExcelToHtmlConverter;
 import org.w3c.dom.Document;
@@ -14,6 +20,21 @@ public class ExcelToPdfConverter {
 		File file = new File(ExcelToPdfConverter.class.getResource("/test.xls").toURI());
 		Document doc = ExcelToHtmlConverter.process(file);
 
+		debugHtml(doc);
+		writePdf(doc);
+	}
+
+	public static void debugHtml(Document doc) throws Exception {
+		DOMSource source = new DOMSource(doc);
+		FileWriter writer = new FileWriter(new File("./test.html"));
+		StreamResult result = new StreamResult(writer);
+
+		TransformerFactory transformerFactory = TransformerFactory.newInstance();
+		Transformer transformer = transformerFactory.newTransformer();
+		transformer.transform(source, result);
+	}
+
+	public static void writePdf(Document doc) throws Exception {
 		FileOutputStream out = new FileOutputStream("./test.pdf");
 		ITextRenderer renderer = new ITextRenderer();
 		renderer.setDocument(doc, null);
