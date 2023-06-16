7934c6beaf069f55ef3ff14854f00669b906006f
aagupt <zinged5@gmail.com>
2023-06-16 13:23:14 +0100

    Update XlsxToPdfConverterTwo.java

--- src/main/java/uk/co/certait/test/XlsxToPdfConverterTwo.java
+++ src/main/java/uk/co/certait/test/XlsxToPdfConverterTwo.java
@@ -1,26 +1,87 @@
 package uk.co.certait.test;
 
-import java.io.FileWriter;
-import java.io.InputStream;
-import java.io.PrintWriter;
+import com.lowagie.text.DocumentException;
+import com.sun.webkit.ContextMenu;
+import org.apache.commons.io.*;
+import org.jsoup.nodes.Entities;
+import org.xhtmlrenderer.layout.SharedContext;
+import org.xhtmlrenderer.pdf.ITextRenderer;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import javax.swing.*;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 
 public class XlsxToPdfConverterTwo {
+    public static Document generateXSSFDoc(String tmpLocation) throws IOException {
+        //        FileInputStream tmpHtml = new FileInputStream(tmpLocation);
+            FileInputStream tmpHtml = new FileInputStream(tmpLocation);
+            String html_string = IOUtils.toString(tmpHtml, StandardCharsets.UTF_8);
+            Document document = Jsoup.parse(html_string, "UTF-8");
+        document.getElementsByClass("column-headers-background").remove();
+        document.getElementsByClass("row-header-wrapper").remove();
+        //    remove excel header /row number
+        document.getElementsByClass("rowHeader").remove();
+        document.getElementsByClass("colHeader").remove();
+        document.outputSettings().syntax(Document.OutputSettings.Syntax.xml);
+        //    add new line after the tables
+        document.getElementsByTag("tbody").append("<br/>");
+//        println("parsing excel workbook to pdf: " + document.toString().replace("text-color","color").replace("&nbsp;"," "))
+        return document;
 
-	public static void main(String[] args) throws Exception {
+    };
 
-		InputStream in = XlsxToPdfConverterTwo.class.getResourceAsStream("/test.xlsx");
-		PrintWriter out = new PrintWriter(new FileWriter("./test-xlsx.html"));
+//    get the pdf output with css style intact
+    public static void htmlToPdf(String tmpLocation, String fileoutlocation){
+    try{
+        FileOutputStream outputStream = new FileOutputStream(fileoutlocation);
+        Document html_doc= generateXSSFDoc(tmpLocation);
+        try {
+            ITextRenderer renderer = new ITextRenderer();
+            SharedContext sharedContext = renderer.getSharedContext();
+            sharedContext.setPrint(true);
+            sharedContext.setInteractive(false);
+            sharedContext.isPaged();
+            renderer.setDocumentFromString(html_doc.toString().replace("text-color","color").replace("&nbsp;"," "));
+            renderer.layout();
+            renderer.createPDF(outputStream);
 
-		// this class is based on code found at
-		// https://svn.apache.org/repos/asf/poi/trunk/src/examples/src/org/apache/poi/ss/examples/html/ToHtml.java
-		// and will convert .xlsx files
-		ExcelToHtmlConverter toHtml = ExcelToHtmlConverter.create(in, out);
-		toHtml.setCompleteHTML(true);
-		toHtml.printPage();
 
-		// rather than writing to file get the HTML in memory and use
-		// FlyingSaucer or OpenHTMlToPdf
+        } catch (DocumentException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if (outputStream != null) outputStream.close();
+        }
 
-		in.close();
-	}
+
+    } catch (IOException e) {
+        throw new RuntimeException(e);
+    }
+
+    }
+
+
+
+    public static void main(String[] args) throws Exception {
+//        add xlsx filepath here
+        String inputFile = "";
+        FileInputStream in = new FileInputStream(inputFile);
+
+        FileOutputStream html_output = new FileOutputStream(inputFile.replace(".xlsx",".html"));
+        FileOutputStream pdf_output = new FileOutputStream(inputFile.replace(".xlsx",".pdf"));
+
+
+        // this class is based on code found at
+        // https://svn.apache.org/repos/asf/poi/trunk/src/examples/src/org/apache/poi/ss/examples/html/ToHtml.java
+        // and will convert .xlsx files
+        ExcelToHtmlConverter toHtml = ExcelToHtmlConverter.create(in, html_output);
+        toHtml.setCompleteHTML(true);
+        toHtml.printPage();
+        in.close();
+        htmlToPdf(inputFile.replace(".xlsx",".html"),inputFile.replace(".xlsx",".pdf"));
+
+
+    }
+
 }
